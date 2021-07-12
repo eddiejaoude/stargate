f42065ced949f02fb9b17b03f280d328d3b2a27e
Krish Gupta <krishdevdb@gmail.com>
2021-07-12 21:05:04 +0530

    feat: modern ping syntax

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -4,7 +4,12 @@ export const ping: CommandInterface = {
   name: "ping",
   description: "Pings the bot.",
   command: async (message) => {
-    await message.reply("Pong!");
-    return;
+    message.reply('Pinging...').then((reply) => {
+      const ping = `${message.channel.type !== 'dm' ? `${message.author},` : ''} Pong!`
+      const roundTrip = `The message round-trip took ${(reply.editedTimestamp || reply.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)}ms.`
+      const ws = `${message.client.ws.ping ? `The heartbeat ping is ${Math.round(message.client.ws.ping)}ms.` : ''}`
+
+      reply.edit(`${ping} ${roundTrip} ${ws}`);
+    })
   },
 };

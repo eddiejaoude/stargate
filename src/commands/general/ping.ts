f3c96859108dec27d062f7217ac25f04de894911
Krish Gupta <krishdevdb@gmail.com>
2021-07-13 08:20:47 +0530

    chore(style): fix code style

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -4,12 +4,12 @@ export const ping: CommandInterface = {
   name: "ping",
   description: "Pings the bot.",
   command: async (message) => {
-    message.reply('Pinging...').then((reply) => {
-      const ping = `${message.channel.type !== 'dm' ? `${message.author},` : ''} Pong!`
-      const roundTrip = `The message round-trip took ${(reply.editedTimestamp || reply.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)}ms.`
-      const ws = `${message.client.ws.ping ? `The heartbeat ping is ${Math.round(message.client.ws.ping)}ms.` : ''}`
+    await const reply = message.reply("Pinging...");
 
-      reply.edit(`${ping} ${roundTrip} ${ws}`);
-    })
+    const ping = `${message.channel.type !== 'dm' ? `${message.author},` : ''} Pong!`;
+    const roundTrip = `The message round-trip took ${(reply.editedTimestamp || reply.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)}ms.`;
+    const ws = `${message.client.ws.ping ? `The heartbeat ping is ${Math.round(message.client.ws.ping)}ms.` : ''}`;
+
+    return reply.edit(`${ping} ${roundTrip} ${ws}`);
   },
 };

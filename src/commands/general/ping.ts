f0686f778b3d87896a74b47cf8e93aabb71fbe3a
Krish Gupta <krishdevdb@gmail.com>
2021-07-13 20:08:45 +0530

    feat: heartbeat roundtrip ping (#132)
    * feat: modern ping syntax
    
    * chore(style): fix code style
    
    * fix: syntax
    
    * fix: typeErrors
    
    * fix: syntax

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -4,7 +4,23 @@ export const ping: CommandInterface = {
   name: "ping",
   description: "Pings the bot.",
   command: async (message) => {
-    await message.reply("Pong!");
+    const reply = await message.reply("Pinging...");
+
+    const ping = `${
+      message.channel.type !== "dm" ? `${message.author},` : ""
+    } Pong!`;
+    const roundTrip = `The message round-trip took ${
+      (reply.editedTimestamp || reply.createdTimestamp) -
+      (message.editedTimestamp || message.createdTimestamp)
+    }ms.`;
+    const ws = `${
+      message.client.ws.ping
+        ? `The heartbeat ping is ${Math.round(message.client.ws.ping)}ms.`
+        : ""
+    }`;
+
+    await reply.edit(`${ping} ${roundTrip} ${ws}`);
+
     return;
   },
 };
